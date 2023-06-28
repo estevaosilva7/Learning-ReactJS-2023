adba9d2a4d784fa8f2192f63d21ce82c29407b8e
Estevão <estevaosilva.sg@gmail.com>
2023-06-28 13:34:57 -0300

    Entendendo o JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+
+  const name = "Estevão" 
+  const newName = name.toUpperCase()
+
+  function soma(a, b) {
+    return a + b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
       <h1> Olá React</h1>
       <p> Meu primeiro App</p>
+      <p> Meu nome é {newName}</p>
+      <p> Soma: {1 + 2} </p>
+      <p> Soma: {soma(7,7)} </p>
+      <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
